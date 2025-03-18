22d07dba5542c06d2a56a903fba785b80ce94739
Rishab <rishabbhardwajdev@gmail.com>
2025-03-18 18:20:15 +0530

    ORM db config

--- src/db-config/db.js
+++ src/db-config/db.js
@@ -2,8 +2,7 @@ const { drizzle } = require("drizzle-orm/better-sqlite3");
 const Database = require("better-sqlite3");
 
 const sqlite = new Database("./projects.db");
+sqlite.pragma("foreign_keys = ON");
 const db = drizzle({ client: sqlite });
 
-const result = await db.execute("select 1");
-
-module.exports = result;
+module.exports = db;
